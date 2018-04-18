eb8667d9d3d42effa06e3a8dd8e96219934846c2
Anuj Sachdeva <anujsachdeva100@gmail.com>
2018-04-19 00:00:35 +0530

    Added get, post and delete methods using axios

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,23 +7,53 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+  state = {
+    posts: [],
+    selectedPostId: null,
+    error: false,
+  }
+
   componentDidMount () {
     axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(response => {
-          console.log(response);
-        })
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: "Anuj",
+          }
+        });
+        this.setState({posts: updatedPosts});
+        console.log(updatedPosts);
+      })
+      .catch(error => {
+        this.setState({error: true})
+      })
+  }
+
+  postClickedHandler (id) {
+    this.setState({selectedPostId: id});
   }
 
   render () {
+    let posts = <p style={{textAlign: 'center'}}>Something Went Wrong!!</p>
+    if(!this.state.error) {
+      posts = this.state.posts.map(post => {
+        return <Post 
+                key={post.id} 
+                title={post.title} 
+                author={post.author}
+                clicked={() => this.postClickedHandler(post.id)} />
+      });
+    }
+
     return (
       <div>
         <section className="Posts">
-          <Post />
-          <Post />
-          <Post />
+          {posts}
         </section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId}/>
         </section>
         <section>
           <NewPost />
