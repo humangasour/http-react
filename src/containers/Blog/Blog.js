0c5cf53700fb4c3c91b10682a76c686d5aaccd5a
Anuj Sachdeva <anujsachdeva100@gmail.com>
2018-05-03 22:21:16 +0530

    added some more routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 // import axios from '../../axios';
-import { Route } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
   render () {
@@ -12,13 +14,29 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/new-post">New Post</a></li>
+              <li><NavLink 
+                    to="/" 
+                    exact
+                    activeClassName="selected"
+                    activeStyle={{
+                      color: '#fa923f',
+                      textDecoration: 'underline',
+                    }}>
+                      Home
+                  </NavLink></li>
+              <li><NavLink to={{
+                pathname: '/new-post',
+                hash: '#submit',
+                search: '?quick-submit=true'
+              }}>New Post</NavLink></li>
             </ul>
           </nav>
         </header>
-        <Route path="/" exact render={() => <h1>Home</h1>}/>
-        <Route path="/" render={() => <h1>Homie</h1>}/>
+        <Route path="/" exact component={Posts} />
+        <Switch>
+          <Route path="/new-post" exact component={NewPost} />
+          <Route path="/:id" component={FullPost} />
+        </Switch> 
       </div>
     );
   }
